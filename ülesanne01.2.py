f26b305ec279875692bcbe66cfa7ecd34d377071
Janella225 <133005244+Janella225@users.noreply.github.com>
2023-05-09 13:58:46 +0300

    Add files via upload

---
+++ ülesanne01.2.py
@@ -0,0 +1,5 @@
+aasta = "2020"
+liblikas = "teelehemosaiikliblikas"
+lause_keskosa = "aasta liblikas on"
+lause = "aasta, lause_keskosa, liblikas"
+print(aasta, lause_keskosa, liblikas)
---
+++ ülesanne01.py
@@ -0,0 +1,10 @@
+print("Tere,maailm!")
+print(32 * 57)
+eesnimi = "Peeter"
+perenimi = "Paan"
+vanus = 21
+print(eesnimi + " " + perenimi + "vanus: " + str(vanus))
+eesnimi = "Peeter"
+perenimi = "Paan"
+vanus = 21
+print(eesnimi, perenimi, "vanus:" , vanus)
